8457e9007a88dd766a1a52a3ddad51a01604455b
StephenCahillane <stephencahillane1@gmail.com>
2025-11-17 18:15:30 +0000

    .

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,32 +1,41 @@
 import React from 'react';
 import styles from './Hero.module.css'
+import Navbar from '../Navbar/Navbar';
 const Hero = () => {
+
+
+
     return (
-        <section className={styles.container}>
+        <div className={styles.fullPageContainer}> 
+            <Navbar />
+            <section className={styles.container}>
 
+                <div className={styles.content}>
+                    <h1 className={styles.title}>Hi, I'm Stephen</h1>
+                    <p className={styles.description}>Im a full stack developer with 5 years of Experience using React, Node JS, reach out if youd like to know more! </p>
 
-            <div className={styles.content}>
-                <h1 className={styles.title}>Hi, I'm Stephen</h1>
-                <p className={styles.description}>Im a full stack developer with 5 years of Experience using React, Node JS, reach out if youd like to know more! </p>
-
-                <a href='mailto:myemail@gmail.com' className={styles.contact}>Contact Me</a>
-            </div>
-
-
-
-            <div style={{ width: '50%', display: 'flex', alignItems: 'center', justifyContent: 'center', zIndex: 1}}>
-                <div style={{ width: '325px', height: '325px', borderRadius: '50%', overflow: 'hidden' }}>
-                    <img className={styles.heroImg} src='/me-suit.jpg' alt='Hero Image of me' />
+                    <a href='mailto:myemail@gmail.com' className={styles.contact}>Contact Me</a>
                 </div>
-            </div>
 
 
 
+                <div className={styles.heroWrapper}>
+
+                    <div className={styles.heroImageSpinner}>
+                        <div className={styles.heroImageContainer}>
+                            <img className={styles.heroImg} src="/me-suit.jpg" alt="Hero" />
+                        </div>
+                    </div>
+                </div>
 
 
-            <div className={styles.topBlurb} />
-            <div className={styles.bottomBlurb} />
-        </section>
+
+                <div className={styles.topBlurb} />
+                <div className={styles.bottomBlurb} />
+            </section>
+
+        </div>
+
     );
 }
 
