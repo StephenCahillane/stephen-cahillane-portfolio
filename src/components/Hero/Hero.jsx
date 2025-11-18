28ba24023f92d45655b5d9231314eee3d4e395aa
StephenCahillane <stephencahillane1@gmail.com>
2025-11-18 12:57:58 +0000

    .

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,32 +6,39 @@ const Hero = () => {
 
 
     return (
-        <div className={styles.fullPageContainer}> 
+        <div className={styles.fullPageContainer}>
             <Navbar />
             <section className={styles.container}>
 
                 <div className={styles.content}>
+                    <div className={styles.welcomeBlock}>
+                        <p>Welcome to my Portfolio</p>
+                    </div>
                     <h1 className={styles.title}>Hi, I'm Stephen</h1>
                     <p className={styles.description}>Im a full stack developer with 5 years of Experience using React, Node JS, reach out if youd like to know more! </p>
 
-                    <a href='mailto:myemail@gmail.com' className={styles.contact}>Contact Me</a>
+                    <button className={styles.contact}>
+                        Contact Me
+                        <img src='/right.png' style={{ width: '30px' }} />
+                    </button>
                 </div>
 
 
 
                 <div className={styles.heroWrapper}>
-
-                    <div className={styles.heroImageSpinner}>
+                    <div className={styles.bluePlanet}>
                         <div className={styles.heroImageContainer}>
-                            <img className={styles.heroImg} src="/me-suit.jpg" alt="Hero" />
+                            <img className={styles.heroImg} src='/spaceman (1).jpg' alt="Hero" />
                         </div>
                     </div>
                 </div>
 
 
 
-                <div className={styles.topBlurb} />
-                <div className={styles.bottomBlurb} />
+                <div className={styles.topBlur} />
+                <div className={styles.bottomBlur} />
+
+                <div className={styles.rocket} />
             </section>
 
         </div>
